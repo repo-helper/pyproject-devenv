e6e35d9f9fd0b608bf8cb2053533bc3565fb0ce5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-11-22 14:38:41 +0000

    Bump version v0.1.3 -> v0.1.4

--- pyproject_devenv/__init__.py
+++ pyproject_devenv/__init__.py
@@ -52,7 +52,7 @@ __all__ = ("mkdevenv", "BaseInstallError", "InstallFromFileError", "InstallError
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.3"
+__version__: str = "0.1.4"
 __email__: str = "dominic@davis-foster.co.uk"
 
 virtualenv_version = tuple(map(int, virtualenv.__version__.split('.')[:3]))
